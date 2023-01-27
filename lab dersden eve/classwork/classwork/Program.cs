b3b1086220a3050bce65fef09f6e852049abaa67
Khudiyev Arkan <ArkanHK@code.local>
2023-01-27 13:07:16 +0400

    lab tasks

---
+++ lab dersden eve/classwork/classwork/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            ////Console.WriteLine("Verilmkish N eded");
+            ////var number = Console.ReadLine();
+            ////int N = Convert.ToInt32(number);
+
+            //if (N % 3 == 0 && N % 7 == 0)
+            //{
+            //    Console.WriteLine("N ededi 3 ve ya 7 ye bolunur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("N ededi 3 ve ya 7 ye bolunmur");
+            //}
+
+            //Console.WriteLine("Verilmish N eded");
+            //var Nstring = Console.ReadLine();
+            //int N = Convert.ToInt32(Nstring);
+
+            //Console.WriteLine("Verilmish M eded");
+            //var Mstring = Console.ReadLine();
+            //int M = Convert.ToInt32(Mstring);
+
+            //int sum = 0;
+
+            //if (N % 2 == 0 && M % 2 == 0)
+            //{
+            //    sum = N + M;
+            //    Console.WriteLine(sum);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("N ve ya M cut eded deiller ");
+            //}
+
+
+            //Console.WriteLine("Verilmish N eded");
+            //var Nstring = Console.ReadLine();
+            //int N = Convert.ToInt32(Nstring);
+
+            //Console.WriteLine("Verilmish M eded");
+            //var Mstring = Console.ReadLine();
+            //int M = Convert.ToInt32(Mstring);
+
+            //int count = 0;
+
+            //for (int i = N; i < M; i++)
+            //{
+            //    if (i % 2 !=0)
+            //    {
+            //        count++;
+
+            //    }
+
+
+            //}
+            //Console.WriteLine(count);
+
+
+
+            //int[] numbers = { 2, 4, 5,7,3 };
+
+            //int sum = 0;
+
+            //for (int i =0 ; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        sum += numbers[i];
+            //    }
+
+            //}
+            // Console.WriteLine(sum);
+
+
+
+
+            //int[] numbers = { 4,5,10,11 };
+
+            //int sum = 0;
+
+            //for (int index = 0; index < numbers.Length; index++)
+            //{
+            //    for (int i = 2; i < numbers[index]; i++)
+            //    {
+            //        if (numbers[index] % i == 0)
+            //        {
+            //            sum += numbers[index];
+            //            break;
+            //        }
+            //    }
+
+
+
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+
+        }
+
+       
+
+    }
+}
+
