e8eaece00f6e755a4b1c930130187e06317c4ecd
Khudiyev Arkan <ArkanHK@code.local>
2023-01-30 12:38:16 +0400

    dersde visual

---
+++ dersde visual/Dersde visual/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Dersde_visual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //verilmish ededler siyahisindaki en boyuk ededi tapan programm
+
+            //int[] numbers = { -30,-50,-89,-95,-7};
+            //int maxnumber = numbers[0];
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i]>maxnumber)
+            //    {
+            //        maxnumber = numbers[i];
+            //    }
+            //}
+            //Console.WriteLine(maxnumber);
+
+            //int result = Showhasil(6, 10);
+            //Console.WriteLine(result);
+            
+
+        }
+        
+
+    }
+}
